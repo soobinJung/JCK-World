8e84fbbb5bf9afb703c578531650c4dbe72a25a1
ChyunKim <co1995@naver.com>
2024-02-05 00:17:18 +0900

    tailwind set init

--- world/src/main/fe/src/components/home/HomeView.tsx
+++ world/src/main/fe/src/components/home/HomeView.tsx
@@ -1,6 +1,6 @@
 export default function HomeView() {
   return (
-    <div className="w-[100vw] h-[100vh] bg-primary-500 flex flex-col items-center justify-center">
+    <div className="w-[100vw] h-[100vh] flex flex-col items-center justify-center">
       <h1 className="text-white">Home</h1>
     </div>
   );
--- world/src/main/fe/tailwind.config.js
+++ world/src/main/fe/tailwind.config.js
@@ -12,19 +12,7 @@ module.exports = {
         pretendard: ['var(--pretendard)'],
       },
       colors: {
-        primary: {
-          50: '#eff1fe',
-          100: '#e1e6fe',
-          200: '#c9cffc',
-          300: '#a8aff9',
-          400: '#8585f4',
-          500: '#7a72ee', // main
-          600: '#604be0',
-          700: '#533cc6',
-          800: '#4333a0',
-          900: '#3a317e',
-          950: '#231c4a',
-        },
+        primary: {},
       },
     },
   },
