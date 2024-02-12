df67fdd9788c6fdbefd3db47817827c2db68244f
soobinJung <soobin84786861@gmail.com>
2024-02-12 18:15:14 +0900

    이그노에 application.properties 추가 > DB 정보 때문에

---
+++ world/src/test/java/com/jck/world/api/examination/repository/ExaminationRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.jck.world.api.examination.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ExaminationRepositoryTest {
+
+}
